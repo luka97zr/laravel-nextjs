625c7a9c8a1c11a418c127b22faafb9b410a099c
Luka Trkulja <luka97zr@gmail.com>
2023-11-23 19:36:11 -0600

    Complete docker config for nextjs,laravel and mariadb

--- frontend/src/app/proba/page.js
+++ frontend/src/app/proba/page.js
@@ -1,7 +1,7 @@
 export default function Proba() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>test asdad</p>
+      <p>test \ </p>
     </main>
   )
 }
